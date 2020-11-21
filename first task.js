35a193151565e95d72a5a270dab21f5189ca6f57
Iuliia Tokarevskaia <iuliia.tokarevskaia@socialbakers.com>
2020-11-21 16:14:10 +0100

    Проверено

--- first task.js
+++ first task.js
@@ -4,13 +4,13 @@ let dog = {
 }
 
 function getProperty (dog){
-    for( let key in dog){
+    for(let key in dog){
         if (dog.hasOwnProperty(key)){
-            console.log(Object.entries(dog));
-
+            console.log(`Ключ - ${key}, значение - ${dog[key]}`);
         }
 
     }
-
 }
 getProperty (dog);
+
+// Не совсем верно сделан вывод. У вас на каждой итерации выводятся все свойства объекта сразу. Нужно выводить по одному на каждой итерации 
